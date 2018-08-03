bebf30f73947fa148d9c50df7859efc2e083202d
Stephen Reddekopp <stephen.reddekopp@smrxt.com>
2018-08-03 15:09:58 -0500

    Get the basic project to load

--- examples/elm/Components/WidgetB/View.elm
+++ examples/elm/Components/WidgetB/View.elm
@@ -2,7 +2,7 @@ module Components.WidgetB.View exposing (..)
 
 import Components.WidgetB.Model exposing (..)
 import Components.WidgetB.Update exposing (..)
-import Html exposing (Html, div, button, text)
+import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 
@@ -10,6 +10,6 @@ view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model) ]
+        , div [] [ text (String.fromInt model) ]
         , button [ onClick Increment ] [ text "+" ]
         ]
